4833537dc1e43ae8971ef9324d8160a7fc7aef41
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-01-07 16:33:35 +0100

    Fix affectation error

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def load_long_description():
         content = readme.read()
     
     for icon, icon_url in GITHUB_ICONS_URLS.items():
-        content.replace(icon, f'<img src="{icon_url}">')
+        content = content.replace(icon, f'<img src="{icon_url}">')
 
     return content
 
